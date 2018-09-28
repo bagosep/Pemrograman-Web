d25e2e407b318623ac0e315d3d252c60d7511789
bagos97 <bagusekoprasetyo97@gmail.com>
2018-09-29 05:27:05 +0700

    Fix View One Page My Profile

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php
+	include("header.php");
+?>
+	<section>
+		<h1 style="margin-left: 0%"><u>Data Pribadi</u></h1>
+		<div class="content-table">
+			<table width="60%" style="font-size:14px; border-radius: 8px; border:1px solid #64abff">
+	        <tr>
+	            <td rowspan="7"><img src="img/foto-copy.png" class="img-foto-profil" alt="" style="width: 180px; height: 220px; margin-left:7%"/></td>
+	        </tr>    
+	          <tr>
+	            <td>Nama</td>
+	            <td>Bagus Eko Prasetyo</td>  
+	                      
+	          </tr>
+	          <tr>
+	            <td>NPM</td>
+	            <td>06.2016.1.06628</td>          
+	          </tr>
+	          <tr>
+	            <td>Jurusan</td>
+	            <td>Informatika</td>          
+	          </tr>
+	          <tr>
+	            <td>Tanggal Lahir</td>
+	            <td>23 - Agustus - 1997</td>          
+	          </tr>
+	          <tr>
+	           <td><button>Info Lebih Lanjut</button></td>  
+	           <td><button>Biodata Lainnya</button></td>          
+	          </tr>
+	         
+	      	</table>		
+		</div>
+		
+	</section>
+<?php
+ include("footer.php");
+?>
