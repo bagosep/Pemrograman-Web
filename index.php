2a8b24eef7fb181d00f57b30676e60cbcb9644ce
bagos97 <bagusekoprasetyo97@gmail.com>
2018-09-29 12:40:51 +0700

    Add view Pop UP

--- index.php
+++ index.php
@@ -26,11 +26,55 @@
 	            <td>23 - Agustus - 1997</td>          
 	          </tr>
 	          <tr>
-	           <td><button>Info Lebih Lanjut</button></td>  
+	           <td><button> <a href="#popup" class="popup-link">Info Lebih Lanjut</a></button></td>  
 	           <td><button>Biodata Lainnya</button></td>          
 	          </tr>
 	         
 	      	</table>		
+		</div>
+		<div class="popup-wrapper" id="popup">
+			<div class="popup-container">
+				<table width="100%" style="font-size:14px; border-radius: 8px; border:1px solid #64abff">
+			        <tr>
+			            <td rowspan="10"><img src="img/foto-copy.png" class="img-foto-profil" alt="" style="width: 180px; height: 220px; margin-left:7%"/></td>
+			        </tr>    
+			          <tr>
+			            <td class="font-popup">Nama</td>
+			            <td class="font-popup">Bagus Eko Prasetyo</td>  
+			                      
+			          </tr>
+			          <tr>
+			            <td class="font-popup">NPM</td>
+			            <td class="font-popup">06.2016.1.06628</td>          
+			          </tr>
+			          <tr>
+			            <td class="font-popup">Jurusan</td>
+			            <td class="font-popup">Informatika</td>          
+			          </tr>
+			          <tr>
+			            <td class="font-popup">Tanggal Lahir</td>
+			            <td class="font-popup">23 - Agustus - 1997</td>          
+			          </tr> 
+			          <tr>
+			            <td class="font-popup">Jenis Kelamin </td>
+			            <td class="font-popup">Laki - Laki </td>          
+			          </tr>  
+			          <tr>
+			            <td class="font-popup">Alamat</td>
+			            <td class="font-popup">JL. Mleto 1/12C</td>          
+			          </tr>  
+			          <tr>
+			            <td class="font-popup">NO. Telp</td>
+			            <td class="font-popup">083857465006</td>          
+			          </tr>  
+			          <tr>
+			            <td class="font-popup">Hobby </td>
+			            <td class="font-popup">Nge Game</td>          
+			          </tr>  
+			           
+	      	</table>	
+				<a class="popup-close" href="#closed">X</a>
+			</div>
 		</div>
 		
 	</section>
